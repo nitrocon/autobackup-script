e3e079e8b30d611827f7acf2e87321c1f4907ee6
nitrocon <77290402+nitrocon@users.noreply.github.com>
2022-11-29 15:47:08 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 rsync pool@31.187.74.53:/ -aAXvh \
 --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} \
-/home/pool/backups/
-cd /home/pool/
+/root/backups/
+cd /root/
 zip -r backups.zip backups
