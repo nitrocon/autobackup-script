8de0e5c4ac8bac9487e955036953277a7b213dfc
nitrocon <77290402+nitrocon@users.noreply.github.com>
2022-11-29 16:14:22 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 echo 'PUBLIC_IP='"${PUBLIC_IP}"'
 read -e -p "User name to backup : " user
-rsync '"$user}"'@'"${PUBLIC_IP}"':/ -aAXvh \
+rsync '"${user}"'@'"${PUBLIC_IP}"':/ -aAXvh \
 --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} \
 /root/backups/
 cd /root/
 zip -r backups.zip backups
+echo -e "$GREEN Finish !!! $COL_RESET"
