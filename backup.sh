562565de5933d72d9b119cda2e8c6661b3ab897d
nitrocon <77290402+nitrocon@users.noreply.github.com>
2023-02-17 22:18:58 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -111,11 +111,11 @@ echo
 sleep 3
 
 # Create backup directory in user's home directory
-backup_path="/~/backup"
+backup_path="~/backup"
 mkdir -p "$backup_path"
 
 # Backup user's home directory using rsync
-sudo rsync -aAXv --delete -e "ssh -i ~/.ssh/autobackup-script" "/~/backup/" "$USER@$IP:/~/backup/backup" \
+sudo rsync -aAXv --delete -e "ssh -i ~/.ssh/autobackup-script" "~/backup/" "$USER@$IP:~/backup" \
 --exclude={"/home/$USER/backup","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/sbin/*","/media/*","/lost+found","/usr/bin/*","/usr/share/*"}
 
 # Create timestamp for backup file name
@@ -129,7 +129,7 @@ echo
 sleep 3
 
 # Zip backup directory
-zip_file="/~/backup/${USER}_backup_${timestamp}.zip"
+zip_file="~/backup/${USER}_backup_${timestamp}.zip"
 sudo zip -r "$zip_file" "$backup_path"
 
 # Remove backup directory
